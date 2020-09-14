07083dcdc54b8444ca897ece0bd4750f6ef3fc06
riatitova <mary.titova.1995@gmail.com>
2020-09-14 19:57:34 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,36 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let symb = '';
+    let ch = [];
+    let letter;
+    let str = '';
+    for (let i = 0; i < expr.length; i++) {
+        symb = expr.slice(0, 10);
+        for (let j = 0; j < 10; j++) {
+            if (symb[j] == 1) {
+                symb[j + 1] == 0 ? ch.push('.') : ch.push('-');
+                j++; 
+            }
+            if (symb[j] == '*') {
+                ch.push('');
+                j+=9;
+            }
+        }
+        symb = ch.join('');
+        if (symb == '') {
+            str += ' ';
+        } else {
+            letter = MORSE_TABLE[symb];
+            str += letter;
+        }
+        ch = [];
+
+        expr = expr.slice(10, expr.length);
+        i--;
+    }
+
+    return str;
 }
 
 module.exports = {
